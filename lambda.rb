1d76c80a2cf24a5594b7bd88ca798cd2e1831097
Leo Cassarani <me@leocassarani.net>
2017-03-21 19:21:46 +0000

    Implement E-App2

--- lambda.rb
+++ lambda.rb
@@ -22,9 +22,15 @@ module Lambda
   def eval(term)
     case term
     when App
-      if term.left.is_a?(Abs) && term.right.is_a?(Abs)
+      if term.left.is_a?(Abs) && term.right.is_a?(Abs) # E-AppAbs
         replace(param: term.left.param, with: term.right, in: term.left.body)
+      elsif term.left.is_a?(Abs) # E-App2
+        App.new(term.left, eval(term.right))
+      else
+        raise "not implemented (App)"
       end
+    else
+      raise "not implemented (not App)"
     end
   end
 
@@ -50,6 +56,11 @@ id = Abs.new("x", Var.new("x"))
 # id id
 id_app = App.new(id, id)
 Lambda.eval(id_app)
+
+# id (id (λz. id z)) -> id (λz. id z)
+puts Lambda.eval(
+  App.new(id, App.new(id, Abs.new("z", App.new(id, Var.new("z")))))
+)
 
 raise unless Lambda.replace(param: "x", with: Var.new("y"), in: Var.new("x")) == Var.new("y")
 raise unless Lambda.replace(param: "x", with: Var.new("y"), in: Var.new("z")) == Var.new("z")
